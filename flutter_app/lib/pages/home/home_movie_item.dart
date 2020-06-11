b496d57bc9a321d84dac172246827b54225dd7e4
孙琦 <993473797@qq.com>
2020-06-11 12:42:03 +0800

    update

--- flutter_app/lib/pages/home/home_movie_item.dart
+++ flutter_app/lib/pages/home/home_movie_item.dart
@@ -61,12 +61,14 @@ class HYHomeMovieItem extends StatelessWidget {
   }
 
   Widget buildContentInfo() {
-    return Column(
-      children: [
-        buildContentInfoTitle(),
-        buildContentInfoRate(),
-        buildContentInfoDesc()
-      ],
+    return Expanded(
+      child: Column(
+        children: [
+          buildContentInfoTitle(),
+          buildContentInfoRate(),
+          buildContentInfoDesc()
+        ],
+      ),
     );
   }
 
@@ -74,32 +76,98 @@ class HYHomeMovieItem extends StatelessWidget {
     return Text.rich(
       TextSpan(
         children: [
-          WidgetSpan(),
-          WidgetSpan(),
-          WidgetSpan()
+          WidgetSpan(
+            child: Icon(
+              Icons.play_circle_outline,
+              color: Colors.pink,
+              size: 40,
+            ),
+            baseline: TextBaseline.ideographic,
+            alignment: PlaceholderAlignment.middle
+          ),
+          TextSpan(
+            text: movie.title
+          ),
+          WidgetSpan(
+            child: Text("(${movie.playDate})"),
+            style: TextStyle(fontSize: 18, color: Colors.grey),
+            alignment: PlaceholderAlignment.bottom
+          )
         ]
       )
     );
   }
 
   Widget buildContentInfoRate() {
-    return Container();
+    return Row(
+      children: [
+        HYStarRating(
+          rating: movie.rating,
+          size: 20,
+        ),
+        Text("${movie.rating}", style: TextStyle(fontSize: 16),)
+      ],
+    );
   }
 
   Widget buildContentInfoDesc() {
-    return Container();
+
+    final genresString = movie.genres.join(" ");
+    final directorString = movie.director.name;
+    List<Actor> casts = movie.casts;
+    final actorString = movie.casts.map((e) => e.name).join(" ");
+
+    return Text(
+      "$genresString / $directorString / $actorString",
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontSize: 16),
+    );
   }
 
   Widget buildContentLine() {
-    return Container();
+    return Container(
+      child: HYDashLine(
+        axis: Axis.vertical,
+        dashWidth: .4,
+        dashHeight: 6,
+        count: 10,
+        color: Colors.pink,
+      ),
+    );
   }
 
   Widget buildContentWish() {
-    return Container();
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/home/wish.png"),
+          Text(
+            "想看",
+            style: TextStyle(
+              fontSize: 18,
+              color: Color.fromARGB(255, 235, 170, 60)
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget buildFooter() {
-    return Text("footer");
+    return Container(
+      width: double.infinity, // 占满整个屏幕
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Color(0xfff2f2f2),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Text(
+        movie.originalTitle,
+        style: TextStyle(fontSize: 20, color: Color(0xff666666)),
+      ),
+    );
   }
 
 }
